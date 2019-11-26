0a1d986a5f5a19622986e63865d9aa71a4ae92d0
“Kgotso-Koete” <kgkoete@gmail.com>
2019-11-26 16:50:09 +0200

    Add Sign In & Sign Out

--- foodtasker/settings.py
+++ foodtasker/settings.py
@@ -117,3 +117,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = '/'
--- foodtaskerapp/views.py
+++ foodtaskerapp/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {})
+    return redirect(restaurant_home)
+
+
+@login_required(login_url='/restaurant/sign-in/')
+def restaurant_home(request):
+    return render(request, 'restaurant/home.html', {})
