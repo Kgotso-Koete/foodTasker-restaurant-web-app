5fbd707eb58ede72594d182017da377f42c61336
“Kgotso-Koete” <kgkoete@gmail.com>
2019-11-28 21:33:37 +0200

    Deploy to Heroku

--- foodtasker/settings.py
+++ foodtasker/settings.py
@@ -118,8 +118,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 LOGIN_REDIRECT_URL = '/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
