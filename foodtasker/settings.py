bab21c9a441c8a5c90a702429e83aa920bf187c8
“Kgotso-Koete” <kgkoete@gmail.com>
2019-12-02 17:36:18 +0200

    Add Customer and Driver database models and their Facebook authorizations

--- foodtasker/settings.py
+++ foodtasker/settings.py
@@ -152,3 +152,16 @@ SOCIAL_AUTH_FACEBOOK_SECRET = FACEBOOK_APP_SECRET
 # Define SOCIAL_AUTH_FACEBOOK_SCOPE to get extra permissions from facebook. Email is not sent by default, to get it, you must request the email permission:
 SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
 SOCIAL_AUTH_FACEBOOK_PROFILE_EXTRA_PARAMS = {'fields': 'id,name,email'}
+
+SOCIAL_AUTH_PIPELINE = (
+    'social.pipeline.social_auth.social_details',
+    'social.pipeline.social_auth.social_uid',
+    'social.pipeline.social_auth.auth_allowed',
+    'social.pipeline.social_auth.social_user',
+    'social.pipeline.user.get_username',
+    'social.pipeline.user.create_user',
+    'foodtaskerapp.social_auth_pipeline.create_user_by_type',  # <--- set the path to the function
+    'social.pipeline.social_auth.associate_user',
+    'social.pipeline.social_auth.load_extra_data',
+    'social.pipeline.user.user_details',
+)
--- foodtaskerapp/admin.py
+++ foodtaskerapp/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from foodtaskerapp.models import Restaurant
+from foodtaskerapp.models import Restaurant, Customer, Driver
 
 admin.site.register(Restaurant)
+admin.site.register(Customer)
+admin.site.register(Driver)
