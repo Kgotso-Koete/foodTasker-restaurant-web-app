2fc13b94a4d738ec8555b8e186e58a32010b4b9e
“Kgotso-Koete” <kgkoete@gmail.com>
2020-02-01 21:43:16 +0200

    Add rest_framework to installed apps

--- foodtasker/settings.py
+++ foodtasker/settings.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.admin', 'django.contrib.auth',
     'django.contrib.contenttypes', 'django.contrib.sessions',
     'django.contrib.messages', 'django.contrib.staticfiles', 'foodtaskerapp',
-    'oauth2_provider', 'social.apps.django_app.default',
+    'rest_framework', 'oauth2_provider', 'social.apps.django_app.default',
     'rest_framework_social_oauth2', 'bootstrap3'
 ]
 
